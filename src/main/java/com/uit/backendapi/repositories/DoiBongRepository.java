e3e911c639753d7d77cf69374b653240d84b0b90
d3m0l15h <davicmax123@gmail.com>
2024-09-23 13:13:38 +0700

    feat(DoiBong): DoiBong's JPA model

---
+++ src/main/java/com/uit/backendapi/repositories/DoiBongRepository.java
@@ -0,0 +1,7 @@
+package com.uit.backendapi.repositories;
+
+import com.uit.backendapi.api.model.DoiBong;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DoiBongRepository extends JpaRepository<DoiBong, Long> {
+}
