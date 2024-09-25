9a562c0be05ed33d93c048b615c8b54bb987a8b4
d3m0l15h <davicmax123@gmail.com>
2024-09-25 23:46:18 +0700

    feat(MuaGiai): config Repository

--- src/main/java/com/uit/backendapi/mua_giai/MuaGiaiRepository.java
+++ src/main/java/com/uit/backendapi/mua_giai/MuaGiaiRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface MuaGiaiRepository extends JpaRepository<MuaGiai, Long> {
-    List<MuaGiai> findByTenMuaGiaiContaining(String tenMuaGiai);
+    MuaGiai findByNamContaining(String nam);
 }
