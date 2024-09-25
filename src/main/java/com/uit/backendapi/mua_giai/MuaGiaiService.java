5afab01e8f7442cd3ba6b6c001c14f81cbc3f8df
d3m0l15h <davicmax123@gmail.com>
2024-09-25 16:12:44 +0700

    feat(MuaGiai): MuaGiai service

--- src/main/java/com/uit/backendapi/mua_giai/MuaGiaiService.java
+++ src/main/java/com/uit/backendapi/mua_giai/MuaGiaiService.java
@@ -1,13 +1,16 @@
 package com.uit.backendapi.mua_giai;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class MuaGiaiService {
+
     private final MuaGiaiRepository muaGiaiRepository;
 
+    @Autowired
     public MuaGiaiService(MuaGiaiRepository muaGiaiRepository) {
         this.muaGiaiRepository = muaGiaiRepository;
     }
@@ -17,10 +20,26 @@ public class MuaGiaiService {
     }
 
     public MuaGiai getMuaGiaiById(Long id) {
-        return muaGiaiRepository.findById(id).orElse(null);
+        return muaGiaiRepository.findById(id).orElseThrow(() ->
+                new RuntimeException("Mua giai not found with id: " + id)
+        );
     }
 
     public MuaGiai createMuaGiai(MuaGiai muaGiai) {
         return muaGiaiRepository.save(muaGiai);
+    }
+
+    public MuaGiai updateMuaGiai(Long id, MuaGiai muaGiaiDto) {
+        return muaGiaiRepository.findById(id)
+                .map(muaGiai -> {
+                    muaGiai.setNam(muaGiaiDto.getNam());
+                    muaGiai.setDoiVoDich(muaGiaiDto.getDoiVoDich());
+                    return muaGiaiRepository.save(muaGiai);
+                })
+                .orElseGet(() -> muaGiaiRepository.save(muaGiaiDto));
+    }
+
+    public void deleteMuaGiai(Long id) {
+        muaGiaiRepository.deleteById(id);
     }
 }
