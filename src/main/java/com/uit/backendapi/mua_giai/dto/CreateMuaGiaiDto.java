132968206b151c8f2b58920a7c53dcd33d5fe517
d3m0l15h <davicmax123@gmail.com>
2024-10-01 15:21:55 +0700

    feat(MuaGiai): Add DTO

---
+++ src/main/java/com/uit/backendapi/mua_giai/dto/CreateMuaGiaiDto.java
@@ -0,0 +1,9 @@
+package com.uit.backendapi.mua_giai.dto;
+
+import lombok.Data;
+
+@Data
+public class CreateMuaGiaiDto {
+    private String nam;
+    private Long doiVoDich;
+}
---
+++ src/main/java/com/uit/backendapi/mua_giai/dto/UpdateMuaGiaiDto.java
@@ -0,0 +1,10 @@
+package com.uit.backendapi.mua_giai.dto;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(callSuper = false)
+public class UpdateMuaGiaiDto extends CreateMuaGiaiDto{
+
+}
