ce5cb818e3e9b07bb77c4e227a70a0ad3ae9c9f1
d3m0l15h <davicmax123@gmail.com>
2024-10-03 10:36:12 +0700

    fix(DoiBong): fix DoiBong update service

--- src/main/java/com/uit/backendapi/Utils.java
+++ src/main/java/com/uit/backendapi/Utils.java
@@ -21,19 +21,4 @@ public class Utils {
 
         return filePath.toString();
     }
-
-    public static void copyNonNullProperties(Object src, Object target) {
-        Field[] fields = src.getClass().getDeclaredFields();
-        for (Field field : fields) {
-            try {
-                field.setAccessible(true);
-                Object value = field.get(src);
-                if (value != null) {
-                    field.set(target, value);
-                }
-            } catch (IllegalAccessException e) {
-                throw new RuntimeException(e);
-            }
-        }
-    }
 }
