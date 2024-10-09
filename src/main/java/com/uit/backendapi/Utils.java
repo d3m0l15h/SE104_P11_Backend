50a169e75df5dfd9692c6fe982f1213c5e2499a5
d3m0l15h <davicmax123@gmail.com>
2024-10-09 19:20:18 +0700

    add: add copyNonNullProperties util

--- src/main/java/com/uit/backendapi/Utils.java
+++ src/main/java/com/uit/backendapi/Utils.java
@@ -1,12 +1,17 @@
 package com.uit.backendapi;
 
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.BeanWrapper;
+import org.springframework.beans.BeanWrapperImpl;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.lang.reflect.Field;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Utils {
 
@@ -20,5 +25,25 @@ public class Utils {
         Files.write(filePath, file.getBytes());
 
         return filePath.toString();
+    }
+
+    public static void copyNonNullProperties(Object source, Object destination, String... ignoreProperties) {
+        Set<String> ignorePropertiesSet = getNullPropertyNames(source);
+        if (ignoreProperties != null) {
+            Collections.addAll(ignorePropertiesSet, ignoreProperties);
+        }
+        BeanUtils.copyProperties(source, destination, ignorePropertiesSet.toArray(new String[0]));
+    }
+
+    public static Set<String> getNullPropertyNames(Object source) {
+        final BeanWrapper src = new BeanWrapperImpl(source);
+        java.beans.PropertyDescriptor[] pds = src.getPropertyDescriptors();
+        Set<String> emptyNames = new HashSet<>();
+        for (java.beans.PropertyDescriptor pd : pds) {
+            // Check if value of this property is null then add it to the collection
+            Object srcValue = src.getPropertyValue(pd.getName());
+            if (srcValue == null) emptyNames.add(pd.getName());
+        }
+        return emptyNames;
     }
 }
