8c196162ad6b8455fa20eebd5ddf180baf864eaf
d3m0l15h <davicmax123@gmail.com>
2024-10-09 19:21:39 +0700

    refactor(LichThiDau): refactor LichThiDau Dto

--- src/main/java/com/uit/backendapi/lich/dto/CreateLichThiDauDto.java
+++ src/main/java/com/uit/backendapi/lich/dto/CreateLichThiDauDto.java
@@ -8,15 +8,11 @@ import java.time.LocalTime;
 
 @Data
 public class CreateLichThiDauDto {
-    String vongThiDau;
-
-    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
-    LocalDate ngayThiDau;
-
-    @DateTimeFormat(iso = DateTimeFormat.ISO.TIME)
-    LocalTime gioThiDau;
-    String sanThiDau;
-    Long maDoiNha;
-    Long maDoiKhach;
-    Long maMuaGiai;
+    private String vongThiDau;
+    private LocalDate ngayThiDau;
+    private LocalTime gioThiDau;
+    private String sanThiDau;
+    private Long maDoiNha;
+    private Long maDoiKhach;
+    private Long maMuaGiai;
 }
