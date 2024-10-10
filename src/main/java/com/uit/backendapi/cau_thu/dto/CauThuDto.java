aa59ae4816989845bcf61348c719d2b1935e063c
d3m0l15h <davicmax123@gmail.com>
2024-10-10 15:55:16 +0700

    refactor

--- src/main/java/com/uit/backendapi/cau_thu/dto/CauThuDto.java
+++ src/main/java/com/uit/backendapi/cau_thu/dto/CauThuDto.java
@@ -1,17 +1,21 @@
 package com.uit.backendapi.cau_thu.dto;
 
+import com.uit.backendapi.ban_thang.BanThang;
 import com.uit.backendapi.doi_bong.dto.DoiBongDto;
+import com.uit.backendapi.doi_bong.dto.DoiBongSimpleDto;
+import com.uit.backendapi.the_phat.ThePhat;
 import lombok.Data;
-import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.Set;
 
 @Data
-@NoArgsConstructor
 public class CauThuDto {
     private Long id;
     private String tenCauThu;
-    private String ngaySinh;
+    private LocalDate ngaySinh;
     private String loaiCauThu;
-    private DoiBongDto maDoi;
+    private DoiBongSimpleDto maDoi;
     private Integer soAo;
     private String viTri;
     private String noiSinh;
---
+++ src/main/java/com/uit/backendapi/cau_thu/dto/CauThuSimpleDto.java
@@ -0,0 +1,14 @@
+package com.uit.backendapi.cau_thu.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+public class CauThuSimpleDto {
+    private Long id;
+    private String tenCauThu;
+    private String loaiCauThu;
+    private Integer soAo;
+    private String viTri;
+    private String quocTich;
+}
