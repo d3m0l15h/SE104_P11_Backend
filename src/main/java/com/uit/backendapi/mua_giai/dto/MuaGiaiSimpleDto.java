e2512163ff54519c6817e948b9c27d58e76fd227
d3m0l15h <davicmax123@gmail.com>
2024-10-10 15:09:52 +0700

    refactor(MuaGiai): refactor MuaGiaiDto

--- src/main/java/com/uit/backendapi/mua_giai/dto/MuaGiaiSimpleDto.java
+++ src/main/java/com/uit/backendapi/mua_giai/dto/MuaGiaiSimpleDto.java
@@ -3,7 +3,7 @@ package com.uit.backendapi.mua_giai.dto;
 import lombok.Data;
 
 @Data
-public class MuaGiaiDto  {
+public class MuaGiaiSimpleDto {
     private Integer id;
     private String nam;
 }
